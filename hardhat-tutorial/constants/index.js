7cc3c1334fa7a903eee8b790de9c7efc2f89b583
Tiago Silva <tfmsilva26@gmail.com>
2022-07-15 23:27:29 +0100

    ICO project

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x1bE1a62E1A09E8E6BE5a71618ce8f53de5770273";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
